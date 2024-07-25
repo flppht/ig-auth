0a89bb54c4c08452052c420faf8ca249619657af
filip <filip.kulasinac@singularity.is>
2024-07-25 14:23:28 +0200

    test

--- server.js
+++ server.js
@@ -6,28 +6,32 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const redirectUri = `${process.env.BASE_URL}${process.env.REDIRECT_PATH}`;
 
+console.log(redirectUri);
 app.get("/auth", (req, res) => {
-  const instagramAuthUrl = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code&state=1`;
+  console.log("uslo u auth");
+  const instagramAuthUrl = `https://api.instagram.com/oauth/authorize?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://ig-auth.onrender.com/auth/callback&scope=user_profile,user_media&response_type=code&state=1`;
   res.redirect(instagramAuthUrl);
 });
 
 app.get("/auth/callback", async (req, res) => {
+  console.log("uslo u callback");
   const { code } = req.query;
+  console.log(code);
   try {
     // Exchange code for a short-lived access token
+    console.log("uslo u try");
     const tokenResponse = await axios.post(
       "https://api.instagram.com/oauth/access_token",
       {
         client_id: process.env.INSTAGRAM_CLIENT_ID,
         client_secret: process.env.INSTAGRAM_CLIENT_SECRET,
         grant_type: "authorization_code",
-        redirect_uri: redirectUri,
+        redirect_uri: "https://ig-auth.onrender.com/auth/callback",
         code,
       }
     );
 
     const { access_token } = tokenResponse.data;
-
     // Exchange short-lived access token for a long-lived token
     const longLivedTokenResponse = await axios.get(
       `https://graph.instagram.com/access_token`,
@@ -41,7 +45,6 @@ app.get("/auth/callback", async (req, res) => {
     );
 
     const { access_token: longLivedToken } = longLivedTokenResponse.data;
-
     // Store the long-lived token and use it for further requests
     // Here, we're just sending it as a response for demonstration purposes
     res.json({ longLivedToken });
